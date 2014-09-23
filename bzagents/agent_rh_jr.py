143f41f7d564cc423ea7d0f49fd92e1abb74ca0d
James Rowe <jroweboy@gmail.com>
2014-09-23 11:57:48 -0600

    Simple motion is in play now. Need to add obstacle fields though

--- bzagents/agent_rh_jr.py
+++ bzagents/agent_rh_jr.py
@@ -2,7 +2,9 @@ import sys
 import math
 import time
 
+from random import randint
 from bzrc import BZRC, Command
+from potential_field import GoalField, ObstacleField, TangentialField
 
 class Agent(object):
     """Class handles all command and control logic for a teams tanks."""
@@ -14,9 +16,10 @@ class Agent(object):
         self.bases = self.bzrc.get_bases()
         self.commands = []
         self.mytanks = [tank for tank in self.bzrc.get_mytanks()]
-        self.setup_common_potential_fields()
+        # self.setup_common_potential_fields()
         for idx, tank in enumerate(self.mytanks):
             self.mytanks[idx].role = None
+            self.mytanks[idx].field = None
 
     def tick(self, time_diff):
         """Some time has passed; decide what to do next."""
@@ -28,11 +31,14 @@ class Agent(object):
                         self.constants['team']]
 
         self.commands = []
+        self.setup_potential_fields()
 
         for idx, tank in enumerate(mytanks):
+            field = self.mytanks[idx].field
             role = self.mytanks[idx].role
             self.mytanks[idx] = tank
             self.mytanks[idx].role = role
+            self.mytanks[idx].field = field
             if tank.status == self.constants['tankdead']:
                self.mytanks[idx].role = None
             elif tank.status == self.constants['tankalive'] and self.mytanks[idx].role == None:
@@ -44,8 +50,25 @@ class Agent(object):
         results = self.bzrc.do_commands(self.commands)
 
     def assign_role(self, idx):
-        # for now...
-        self.mytanks[idx].role = self.attack
+        # randomly assign a role and field
+        # self.mytanks[idx].role = self.attack
+        r = randint(0, 2)
+        if r == 0: # attack role
+            self.mytanks[idx].role = self.attack
+            self.mytanks[idx].field = None
+        else:
+        #elif r == 1 or r ==2: # combined defend and seek for noe 
+            r2 = randint(0, len(self.fields['goal']) - 1)
+            self.mytanks[idx].role = self.seek
+            print r2, self.fields['goal']
+
+            self.mytanks[idx].field = self.fields['goal'][r2]
+
+    def setup_potential_fields(self):
+        self.fields = {}
+        self.fields['goal'] = []
+        for flag in self.flags:
+            self.fields['goal'].append(GoalField(flag.x, flag.y, 25, 50, 0.6))
 
     def attack(self, tank):
         """Find the closest enemy and chase it, shooting as you go."""
@@ -70,6 +93,8 @@ class Agent(object):
 
     def seek(self, tank):
         """Get the flag and defend own flag bearer."""
+        dx, dy = tank.field.calc(tank)
+        self.move_to_position(tank, dx, dy)
         pass
 
     def move_to_position(self, tank, target_x, target_y):
@@ -88,9 +113,6 @@ class Agent(object):
         elif angle > math.pi:
             angle -= 2 * math.pi
         return angle
-
-    def setup_potential_fields(self):
-        self.fields = []
 
 
 def main():
--- rungame.sh
+++ rungame.sh
@@ -1,8 +1,8 @@
 ./bin/bzrflag --world=maps/four_ls.bzw --friendly-fire --red-port=50100 --green-port=50101 --purple-port=50102 --blue-port=50103 $@ &
 sleep 2
 echo "Connect on ports 50100 to 50103"
-#python bzagents/agent0.py localhost 50100 &
-#python bzagents/agent0.py localhost 50101 &
+python bzagents/agent0.py localhost 50100 &
+python bzagents/agent_rh_jr.py localhost 50101 &
 #python bzagents/agent0.py localhost 50102 &
 #python bzagents/agent0.py localhost 50103 &
 
