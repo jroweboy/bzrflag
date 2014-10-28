aceaa4ba7de630ddef5cf3e7084c7c5b44d5262f
robhobbes <robhobbes@gmail.com>
2014-10-27 19:59:56 -0600

    Working on occgrid stuff.

--- runtest.sh
+++ runtest.sh
@@ -1,4 +1,4 @@
-./bin/bzrflag --world=maps/twoteams2.bzw --red-port=50100 --green-port=50103 $@ &
+./bin/bzrflag --world=maps/twoteams2.bzw --red-port=50100 --green-port=50103 --default-true-positive=.97 --default-true-negative=.9 --occgrid-width=100 --no-report-obstacles $@ &
 sleep 3
 echo "Connect on ports 50100 to 50103"
 #python bzagents/agent_rh_jr.py localhost 50100 &
